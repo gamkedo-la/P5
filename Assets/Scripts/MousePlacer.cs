fc4607d4f3908fac9b3bed897b049c228b1bd915
Steve Palley <spall03@gmail.com>
2016-01-13 19:50:03 -0800

    can pick up and put down bottles, added fridge

--- Assets/Scripts/MousePlacer.cs
+++ Assets/Scripts/MousePlacer.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class MousePlacer : MonoBehaviour {
 
-	public GameObject placingObject;
+//	public GameObject placingObject;
 
 	private GameObject activeObject;
+	int clickMask;
 
-	private int alternateShuffle = 0;
-	public Vector3[] sideAdjustTranslation;
+//	private int alternateShuffle = 0;
+//	public Vector3[] sideAdjustTranslation;
 
 	// Use this for initialization
 	void Start () {
 
-		activeObject = GameObject.Instantiate(placingObject);
+	//	activeObject = GameObject.Instantiate(placingObject);
+		clickMask = ~LayerMask.GetMask("Ignore Raycast"); //flip bitmask 
 	
 	}
 	
@@ -21,44 +23,88 @@ public class MousePlacer : MonoBehaviour {
 	void Update () {
 
 		Vector3 mousePos = Input.mousePosition;
-		Debug.Log(mousePos);
-		activeObject.transform.position = mousePos;
+	//	Debug.Log(mousePos);
+	//	activeObject.transform.position = mousePos;
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit rhInfo;
 
-		if (Physics.Raycast(ray, out rhInfo)){ //out causes pass by reference
+		if (Physics.Raycast(ray, out rhInfo, 5000f, clickMask)){ //out causes pass by reference
 
-			Vector3 spawnAt = rhInfo.point;
-			spawnAt += Vector3.up * 1.5f;
+			if (activeObject == null){
 
-			activeObject.transform.position = spawnAt;
+				if (rhInfo.collider.gameObject.CompareTag("PicnicObject") && Input.GetButtonDown("Fire1")){
+			
+					Debug.Log(rhInfo.collider.name);
+
+					activeObject = rhInfo.collider.gameObject;
+					activeObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+					activeObject.GetComponentInParent<Rigidbody>().useGravity = false;
+					activeObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+					activeObject.GetComponent<Collider>().isTrigger = true;
+
+					activeObject.GetComponentInParent<Rigidbody>().velocity = Vector3.zero;
+					activeObject.GetComponentInParent<Rigidbody>().angularVelocity = Vector3.zero;
+
+				}
+
+			}
+			else {
+
+				Vector3 carryPoint = rhInfo.point;
+
+				carryPoint += Vector3.up * 1.5f;
+				activeObject.transform.position = carryPoint;
+
+				if (Input.GetButtonUp("Fire1")){
+
+					activeObject.layer = LayerMask.NameToLayer("Default");
+					activeObject.GetComponentInParent<Rigidbody>().useGravity = true;
+					activeObject.GetComponentInParent<Rigidbody>().isKinematic = false;
+					activeObject.GetComponent<Collider>().isTrigger = false;
+					activeObject = null;
+
+				}
+					
+
+			}
+			
+				
+
+
+
+
+		
+			//	carryPoint += Vector3.up * 1.5f;
+
+		
+			}
+				
 //
 //			activeObject.transform.position += sideAdjustTranslation[alternateShuffle];
 
-			if (Input.GetButtonDown("Fire1")){
-
-				activeObject.GetComponent<Rigidbody>().isKinematic = false;
-				activeObject.GetComponentInChildren<Collider>().isTrigger = false;
-
-				activeObject = (GameObject) GameObject.Instantiate(placingObject,
-					activeObject.transform.position,
-					activeObject.transform.rotation); //put down object
-
-			}
-				
-			if (Input.GetButtonDown("Fire2")){
-
-				activeObject.transform.Rotate(Vector3.up, -90.0f); //rotate object
-
-//				alternateShuffle++;
-//				if(alternateShuffle >= 4) {
-//					alternateShuffle -= 4;
-//				}
-			}
+//			if (Input.GetButtonDown("Fire1")){
+//
+//				activeObject.GetComponent<Rigidbody>().isKinematic = false;
+//				activeObject.GetComponentInChildren<Collider>().isTrigger = false;
+//
+//				activeObject = (GameObject) GameObject.Instantiate(placingObject,
+//					activeObject.transform.position,
+//					activeObject.transform.rotation); //put down object
+//
+//			}
+//				
+//			if (Input.GetButtonDown("Fire2")){
+//
+//				activeObject.transform.Rotate(Vector3.up, -90.0f); //rotate object
+//
+////				alternateShuffle++;
+////				if(alternateShuffle >= 4) {
+////					alternateShuffle -= 4;
+////				}
+//			}
 
 		}
 	
 	}
-
-}
+	
---
+++ Assets/Scripts/RespawnOnExit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnOnExit : MonoBehaviour {
+
+	public GameObject generateThis;
+	public Transform generateAt;
+
+	//private int debt = 0;
+
+	void OnTriggerEnter(Collider col){
+
+	//	debt++;
+
+	}
+
+	void OnTriggerExit(Collider col){
+
+		//debt--;
+
+		//if (debt < 0){
+			
+			GameObject.Instantiate(generateThis, generateAt.position, generateAt.rotation);
+
+		//}
+
+
+	}
+}
